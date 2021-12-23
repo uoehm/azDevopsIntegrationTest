2c8ec28d1d3281a94463860dcd2038d3198174bb
Uwe Oehmichen <u.oehmichen@queo.onmicrosoft.com>
2021-12-23 11:19:54 +0100

    AB#32 The big feature

--- src/Demo.Console/Program.cs
+++ src/Demo.Console/Program.cs
@@ -7,6 +7,7 @@ namespace Demo.Console
         static void Main(string[] args)
         {
             System.Console.WriteLine("Hello World!");
+            System.Console.WriteLine("This is the big feature from Issue 32");
 
             System.Console.WriteLine("enter for exit");
             System.Console.ReadLine();
